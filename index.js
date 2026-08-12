b13595e2ea564a70a49f289d99c1c0a78fb50a44
Dashshare <daianhnguyen206@gmail.com>
2026-08-12 09:34:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const PORT = process.env.PORT || 10000;
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end('Bot Discord AI đang chạy trên Render! 🚀');
+    res.end('Bot Discord AI runningg on Render! 🚀');
 });
 server.listen(PORT, () => {
     console.log(`🌐 Bot is ready ${PORT}`);
